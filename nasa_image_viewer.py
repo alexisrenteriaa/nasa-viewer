7f87b76cb973d0c6ac08f4f5c9d25c2949b494ed
alexisrenteriaa <alexisrenteria02@icloud.com>
2025-07-04 14:32:54 -0700

    Agrega soporte para .env y ocultación de API_KEY

--- nasa_image_viewer.py
+++ nasa_image_viewer.py
@@ -1,27 +1,23 @@
+import os
+from dotenv import load_dotenv
 import requests
 from PIL import Image
 from io import BytesIO
 
-# Reemplaza con tu clave de API de NASA
-API_KEY = 'zd22vaVpowWGsHFkZ3kOXeggdBapJI7NGh2Rl5fe'
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+API_KEY = os.getenv('NASA_API_KEY')
 
 def obtener_imagen_apod():
-    # URL para obtener la imagen del día
     url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
-    
-    # Hacer la petición a la API
     response = requests.get(url)
-    
-    # Comprobar si la respuesta es correcta (código 200)
+
     if response.status_code == 200:
-        # Convertir la respuesta a formato JSON
         data = response.json()
-        
-        # Obtener la URL de la imagen
         imagen_url = data['url']
         print(f"Imagen del día: {imagen_url}")
         
-        # Si la imagen es de tipo JPG, la descargamos
         if imagen_url.endswith('.jpg'):
             img_response = requests.get(imagen_url)
             img = Image.open(BytesIO(img_response.content))
